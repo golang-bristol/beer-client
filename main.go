bb84c9e823ade007ed984ded6f888a8611e7f030
sernle <le.canhson@gmail.com>
2016-10-11 21:51:49 +0100

    Get beers

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 /*
@@ -14,6 +17,33 @@ GET /beer/[:id]/reviews - return a list of reviews for beer
 POST /beer/[:id]/review - create a new review for beer
 */
 
+var requestURL = flag.String("url", "http://localhost:8080/beers", " webserver ")
+var requestMethod = flag.String("method", "GET", "GET/PUT")
+var id = flag.String("id", "", "id")
+var requestData = flag.String("data", "", "post data")
+
 func main() {
-	fmt.Println("Hello world!")
+	flag.Parse()
+	switch strings.ToUpper(*requestMethod) {
+	case "GET":
+		res, err := http.Get(*requestURL)
+		if err != nil {
+			// Request failed.
+			fmt.Println("Error executing HTTP request :", err)
+			return
+		}
+
+		//defer res.Body.Close()
+
+		responseData, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			// Could not read data.
+			fmt.Println("Error reading response data :", err)
+			return
+		}
+		fmt.Println(string(responseData))
+
+	}
+	// Print received data.
+
 }
