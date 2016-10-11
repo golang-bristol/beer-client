c7452b2799eed6ff75ed1592108d0dc684a63055
Anna Winters <anna@winters.ws>
2016-10-11 19:38:16 +0100

    Add comment with the HTTP requests that need to be supported

--- main.go
+++ main.go
@@ -5,6 +5,15 @@ import (
 	"net/http"
 )
 
+/*
+Supported commands:
+GET /beers - return list of beers
+POST /beers - create a new beer
+GET /beer/[:id] - return a single beer
+GET /beer/[:id]/reviews - return a list of reviews for beer
+POST /beer/[:id]/review - create a new review for beer
+*/
+
 func main() {
 	fmt.Println("Hello world!")
 }
